d972ed0e623db00728b98cc1e8adc32573c891d6
Negusbuk <Andreas Mussgiller>
2014-02-06 14:21:38 +0100

    added documentation

---
+++ html/search/all_67.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['g_5f_5fcpp_5fsetup_5finitrootextensionsdict',['G__cpp_setup_initRootExtensionsDict',['../class_g____cpp__setup__init_root_extensions_dict.html',1,'']]],
+  ['g_5f_5fsizep2memfuncrootextensionsdict',['G__Sizep2memfuncRootExtensionsDict',['../class_g_____sizep2memfunc_root_extensions_dict.html',1,'']]]
+];
---
+++ html/search/classes_74.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['tarcarrow',['TArcArrow',['../class_t_arc_arrow.html',1,'']]],
+  ['tmgraph',['TMGraph',['../class_t_m_graph.html',1,'']]],
+  ['tmgraph2d',['TMGraph2D',['../class_t_m_graph2_d.html',1,'']]],
+  ['tmgraph2derrors',['TMGraph2DErrors',['../class_t_m_graph2_d_errors.html',1,'']]],
+  ['tmgrapherrors',['TMGraphErrors',['../class_t_m_graph_errors.html',1,'']]],
+  ['tmpalette',['TMPalette',['../class_t_m_palette.html',1,'']]]
+];
